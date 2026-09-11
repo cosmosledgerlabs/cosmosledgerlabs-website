e06115ea357bcdcb8046a331ec07baf6e744ff9b
cosmosledgerlabs <info@cosmosledgerlabs.com>
2026-09-11 15:39:06 -0700

    Import useEffect in flow.js

--- pages/flow.js
+++ pages/flow.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
