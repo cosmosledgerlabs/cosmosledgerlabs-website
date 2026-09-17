02e4d081bed2f8638e99807d41d679e54f052f93
cosmosledgerlabs <info@cosmosledgerlabs.com>
2026-09-17 11:48:59 -0700

    Add PWA support in _document.js
    Added PWA meta tags and manifest link to enhance mobile experience.

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ export default function Document() {
         {/* Preconnect to Google Fonts hosts to reduce font-load latency / layout shift */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* Add-to-home-screen (PWA) */}
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="apple-mobile-web-app-title" content="COSMOS" />
+        <meta name="mobile-web-app-capable" content="yes" />
       </Head>
       <body>
         <Main />
